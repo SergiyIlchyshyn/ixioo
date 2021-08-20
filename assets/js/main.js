d9050b190d32579b4b7291597aba217da45f7e3e
Sergiy Ilchyshyn <sergiy.ilchyshyn@gmail.com>
2021-08-20 12:09:36 +0300

    updated chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,20 +11,67 @@ linkSidebar.forEach(l => l.addEventListener('click', activeLink));
 
 
 /* Chart.js */
-const data = {
-    labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-    ],
-    datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-    }]
+var options = {
+    chart: {
+        height: 220,
+        type: "radialBar",
+        dropShadow: {
+            enabled: true,
+            enabledSeries: undefined,
+            top: 5,
+            left: 2,
+            blur: 10,
+            opacity: 0.25
+        }
+        },
+        plotOptions: {
+        radialBar: {
+            hollow: {
+            margin: 0,
+            size: "80%",
+            background: "#fff",
+            position: "front"
+            },
+            track: {
+            background: "#e7e7e7",
+            strokeWidth: "97%",
+            margin: 5, // margin is in pixels
+            dropShadow: {
+                enabled: false,
+                top: 0,
+                left: 0,
+                blur: 3,
+                opacity: 0.5
+            }
+            },
+    
+            dataLabels: {
+            showOn: "always",
+            name: {
+                offsetY: -20,
+                show: true,
+                color: "#888",
+                fontSize: "13px"
+            },
+            value: {
+                formatter: function(val) {
+                return 2280;
+                },
+                color: "#111",
+                fontSize: "30px",
+                show: true
+            }
+            }
+        }
+    },
+
+    series: [74],
+    stroke: {
+    lineCap: "round"
+    },
+    labels: ["Goal"]
 };
+
+var chart = new ApexCharts(document.querySelector("#chart-accuracy"), options);
+
+chart.render();
