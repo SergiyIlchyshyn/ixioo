a4f02ae6e6c7d351ef13b91d9a06ec6da493efd7
Sergiy Ilchyshyn <sergiy.ilchyshyn@gmail.com>
2021-08-20 12:26:58 +0300

    updated chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,67 +11,141 @@ linkSidebar.forEach(l => l.addEventListener('click', activeLink));
 
 
 /* Chart.js */
-var options = {
-    chart: {
-        height: 220,
-        type: "radialBar",
-        dropShadow: {
-            enabled: true,
-            enabledSeries: undefined,
-            top: 5,
-            left: 2,
-            blur: 10,
-            opacity: 0.25
-        }
-        },
-        plotOptions: {
+var options1 = {
+	chart: {
+		renderTo: 'container',
+        height: 150,
+        width:100,
+        type: 'radialBar',
+        spacingTop: 0,
+        spacingBottom: 0,
+        spacingLeft: 0,
+        spacingRight: 0,
+        offsetY: 0,
+		toolbar: {
+			show: false
+		},
+	},
+	plotOptions: {
         radialBar: {
+            startAngle: 0,
+            endAngle: 360,
             hollow: {
             margin: 0,
-            size: "80%",
-            background: "#fff",
-            position: "front"
+            size: '65%',
+            background: '#fff',
+            image: undefined,
+            imageOffsetX: 0,
+            imageOffsetY: 0,
+            position: 'front',
+            
             },
             track: {
-            background: "#e7e7e7",
-            strokeWidth: "97%",
-            margin: 5, // margin is in pixels
-            dropShadow: {
-                enabled: false,
-                top: 0,
-                left: 0,
-                blur: 3,
-                opacity: 0.5
-            }
+            background: '#F5F5FF',
+            strokeWidth: '90%',
+            margin: 0, // margin is in pixels
+            
             },
-    
             dataLabels: {
-            showOn: "always",
+            showOn: 'always',
             name: {
-                offsetY: -20,
-                show: true,
-                color: "#888",
-                fontSize: "13px"
+                offsetY: -30,
+                show: false,
+                color: '#003366',
+                fontSize: '17px'
             },
             value: {
                 formatter: function(val) {
-                return 2280;
+                return parseInt(val);
                 },
-                color: "#111",
-                fontSize: "30px",
-                show: true
+                color: '#003366',
+                fontSize: '28px',
+                lineHight: '1',
+                show: true,
+                offsetY: 12,
+                offsetX:0,
+                
             }
             }
         }
-    },
+        },
+        fill: {
+            colors:['#0a84ff']
+        },
+        series: [90],
+        stroke: {
+            lineCap: 'round'
+        },
+}
+var options2 = {
+	chart: {
+		renderTo: 'container',
+        height: 150,
+        width:100,
+        type: 'radialBar',
+        spacingTop: 0,
+        spacingBottom: 0,
+        spacingLeft: 0,
+        spacingRight: 0,
+        offsetY: 0,
+		toolbar: {
+			show: false
+		},
+	},
+	plotOptions: {
+        radialBar: {
+            startAngle: 0,
+            endAngle: 360,
+            hollow: {
+            margin: 0,
+            size: '65%',
+            background: '#fff',
+            image: undefined,
+            imageOffsetX: 0,
+            imageOffsetY: 0,
+            position: 'front',
+            
+            },
+            track: {
+            background: '#F5F5FF',
+            strokeWidth: '75%',
+            margin: 0, // margin is in pixels
+            
+            },
+            dataLabels: {
+            showOn: 'always',
+            name: {
+                offsetY: -30,
+                show: false,
+                color: '#003366',
+                fontSize: '17px'
+            },
+            value: {
+                formatter: function(val) {
+                return parseInt(val);
+                },
+                color: '#003366',
+                fontSize: '28px',
+                lineHight: '1',
+                show: true,
+                offsetY: 12,
+                offsetX:0,
+                
+            }
+            }
+        }
+        },
+        fill: {
+        colors:['#f92e22']
+        },
+        series: [75],
+        stroke: {
+        lineCap: 'round'
+	},
+}
 
-    series: [74],
-    stroke: {
-    lineCap: "round"
-    },
-    labels: ["Goal"]
-};
+var chart1 = new ApexCharts(document.querySelector("#chart-accuracy"), options1);
+var chart2 = new ApexCharts(document.querySelector("#chart-loss"), options2);
 
-var chart = new ApexCharts(document.querySelector("#chart-accuracy"), options);
-
-chart.render();
+chart1.render();
+chart2.render();
